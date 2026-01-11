8f1d47a6c6241d690f75793d59423fb5f3b464e1
Shikamaru <1771683991@qq.com>
2026-01-11 18:10:02 -0500

    update

--- Assets/Starter Assets/Runtime/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/Starter Assets/Runtime/FirstPersonController/Scripts/FirstPersonController.cs
@@ -154,7 +154,7 @@ namespace StarterAssets
 		private void Move()
 		{
 			// set target speed based on move speed, sprint speed and if sprint is pressed
-			float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+			float targetSpeed = MoveSpeed;
 
 			// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
